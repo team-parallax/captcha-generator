be22cb2b3d0b37c72d49a0112ff763850c0025f2
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2022-05-19 15:39:04 +0200

    fix jest eslint config

--- src/__test__/createCaptcha.test.ts
+++ src/__test__/createCaptcha.test.ts
@@ -1,14 +1,14 @@
-it("should generate a valid captcha w/o parameters", async() => {
-    // TO-DO
-    expect(true).toBe(false)
+it('should generate a valid captcha w/o parameters', async () => {
+	// TO-DO
+	expect(true).toBe(false);
 });
 
-it("should generate a valid captcha w/ valid parameters", async() => {
-    // TO-DO
-    expect(true).toBe(false)
+it('should generate a valid captcha w/ valid parameters', async () => {
+	// TO-DO
+	expect(true).toBe(false);
 });
 
-it("should throw an error with invalid parameters", async() => {
-    // TO-DO
-    expect(true).toBe(false)
+it('should throw an error with invalid parameters', async () => {
+	// TO-DO
+	expect(true).toBe(false);
 });
