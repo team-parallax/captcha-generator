a11f9e1ed8a7d6a576557cfd74927629995ebc37
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2022-06-13 10:08:15 +0200

    fix typeerror in exception

--- src/captcha/error.ts
+++ src/captcha/error.ts
@@ -1,7 +1,7 @@
 
 type TCaptchaParameter = 'language' | 'speed' | 'gap' | 'length';
 export class InvalidCaptchaParameterError extends Error {
-	constructor(param: TCaptchaParameter, value: string) {
+	constructor(param: TCaptchaParameter, value: string | number) {
 		super(`the ${param} parameter is invalid: ${value}`);
 	}
 }
